d1259c45ac21c958f773d118feb1243e736623c0
abrahamalanya <alanyaoficial@gmail.com>
2024-10-31 14:47:33 -0500

    feat: halloween

--- resources/views/livewire/frases.blade.php
+++ resources/views/livewire/frases.blade.php
@@ -1,19 +1,20 @@
 @php
     $number = mt_rand(1, 7);
+    $audio = mt_rand(1, 8);
 @endphp
 <section
-    wire:poll.3600000ms="actualizarFrase"
+    wire:poll.1800000ms="actualizarFrase"
     x-data="{ visible: false }" 
     x-init="
         setInterval(() => {
             visible = true;
             $refs.audio.play();
             setTimeout(() => visible = false, 15000);
-        }, 3615000);"
+        }, 1815000);"
     class="absolute bottom-0 right-4">
     <div x-show="visible">
         <audio x-ref="audio" hidden>
-            <source src="{{ asset('img/halloween/halloween.mp3') }}" type="audio/mpeg">
+            <source src="{{ asset('img/halloween/'.$audio.'.mp3') }}" type="audio/mpeg">
         </audio>
         <div class="w-[300px]">
             <span style="transition: all 0.5s;" class="text-2xl font-bold inline-block  text-slate-700 rounded-xl p-1 tracking-wide leading-snug">
