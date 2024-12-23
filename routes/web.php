c4d263a7efaa5a65983ba3dd0e8e2791b122c219
EmersonXR <emersonesquivel222@gmail.com>
2024-12-23 16:43:01 -0500

    feat: Se añadio la ruta del inventario a routers, web

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\FunnelController;
 use App\Http\Controllers\GestionClienteController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImportController;
+use App\Http\Controllers\InventarioController;
 use App\Http\Controllers\ListaUsuarioController;
 use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\ProductoController;
@@ -58,6 +59,8 @@ Route::middleware([
         'reporte_cliente' => ReporteClienteController::class,
         'reporte_cliente_nuevo' => ReporteClienteNuevoController::class,
         'producto' => ProductoController::class,
+        //inventario
+        'inventario' => InventarioController::class,
         // Cliente
         'cliente' => ClienteController::class,
         'cliente-consultor' => ClienteConsultorController::class,
